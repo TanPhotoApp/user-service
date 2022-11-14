b9eae65367872e1b3a278e1562a31c2899991cc4
tanlh <huutanctct@gmail.com>
2022-11-15 02:22:55 +0700

    Config eureka run multiple instances

--- src/main/java/com/photoapp/users/controller/UsersController.java
+++ src/main/java/com/photoapp/users/controller/UsersController.java
@@ -1,15 +1,20 @@
 package com.photoapp.users.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("users")
+@RequiredArgsConstructor
 public class UsersController {
+
+    private final Environment environment;
 
     @GetMapping("status/check")
     public String status() {
-        return "Working";
+        return "Working on port " + environment.getProperty("local.server.port");
     }
 }
