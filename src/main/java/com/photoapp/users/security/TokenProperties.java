c137470365aed36c3cb21214744432c5fefff4bc
tanlh <huutanctct@gmail.com>
2022-11-24 01:38:43 +0700

    Implement refresh scope for token properties bean

--- src/main/java/com/photoapp/users/security/TokenProperties.java
+++ src/main/java/com/photoapp/users/security/TokenProperties.java
@@ -1,6 +1,8 @@
 package com.photoapp.users.security;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @ConfigurationProperties(prefix = "token")
+@RefreshScope
 public record TokenProperties(long expireTime, String signingKey) {}
