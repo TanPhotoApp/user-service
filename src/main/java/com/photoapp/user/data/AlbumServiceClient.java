4b7a86ba8695c44a78314739f8c448158408da0d
tanlh <huutanctct@gmail.com>
2022-11-27 22:27:26 +0700

    Add album circuit breaker

--- src/main/java/com/photoapp/user/data/AlbumServiceClient.java
+++ src/main/java/com/photoapp/user/data/AlbumServiceClient.java
@@ -1,6 +1,7 @@
 package com.photoapp.user.data;
 
 import com.photoapp.user.api.model.AlbumResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,13 @@ import java.util.List;
 public interface AlbumServiceClient {
 
     @GetMapping("users/{id}/albums")
+    @CircuitBreaker(name = "album-service", fallbackMethod = "getAlbumsFallBack")
     List<AlbumResponse> getAlbums(@PathVariable String id);
+
+    default List<AlbumResponse> getAlbumsFallBack(String id, Throwable cause) {
+        System.err.printf("Get albums with id=%s got error: %s%n", id, cause.getMessage());
+
+        return List.of();
+    }
 
 }
