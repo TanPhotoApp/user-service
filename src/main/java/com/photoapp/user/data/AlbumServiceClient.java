67b6c64bf7a2306af1fd477a4e644bf0ec5a53f2
tanlh <huutanctct@gmail.com>
2022-11-28 22:27:34 +0700

    Add retry on album service client

--- src/main/java/com/photoapp/user/data/AlbumServiceClient.java
+++ src/main/java/com/photoapp/user/data/AlbumServiceClient.java
@@ -2,6 +2,7 @@ package com.photoapp.user.data;
 
 import com.photoapp.user.api.model.AlbumResponse;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import java.util.List;
 public interface AlbumServiceClient {
 
     @GetMapping("users/{id}/albums")
+    @Retry(name = "album-service")
     @CircuitBreaker(name = "album-service", fallbackMethod = "getAlbumsFallBack")
     List<AlbumResponse> getAlbums(@PathVariable String id);
 
